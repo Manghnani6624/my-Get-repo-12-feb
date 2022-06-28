e43086fe07a0d17371f2f03a5920331f6424249e
manghnani6624 <manghnaninaveen.com>
2022-06-28 14:33:32 +0530

    my 100% completed

--- myjavaregpage.java
+++ myjavaregpage.java
@@ -1,2 +1,6 @@
 this is my java registration
-this is my manager work is done 
+1st day of java line of code
+2nd day of java line of code
+3th day of java line of code
+4th day of java line of code
+5th day total work done 
