e188bd04a87ac8fb65f8cff3563ef8440c780341
manghnani6624 <manghnaninaveen.com>
2022-07-01 12:15:54 +0530

    code is done

--- myjavaregpage.java
+++ myjavaregpage.java
@@ -1,1 +1,5 @@
-this is my manager work is completed 
+this is my java registration page
+1st day of java line of code 
+2nd day of java line of code
+3rd day of java line of code 
+4th day of java line of code 100% of task completed
